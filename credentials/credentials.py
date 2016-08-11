26737d246e38884630a3ae01a82e9459809b425d
sektionschef <the.stevee@gmail.com>
2016-08-11 21:09:50 +0200

    credentials dummy file added

---
+++ credentials/credentials.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+## get that shit here:  https://apps.twitter.com
+
+CONSUMER_KEY = 'Your-Consumer-Key'
+CONSUMER_SECRET = 'Your-Secret-Key'
+ACCESS_KEY = 'Your-Access-Key'
+ACCESS_SECRET = 'Your-Access-Secret'
