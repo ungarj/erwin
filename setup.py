be37cc79bb07fa90c04e4c8c3ef8b0ce0c19f222
ungarj <joachim.ungar@gmail.com>
2016-08-11 20:40:54 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='erwin',
+    version='0.0.1',
+    description='gags gags gags',
+    author='Joachim Ungar',
+    author_email='joachim.ungar@gmail.com',
+    url='https://github.com/ungarj/erwin',
+    license='MIT',
+    packages=['erwin'],
+    install_requires=[
+        ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Religion',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
