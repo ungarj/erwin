da459d9f82919228bd87f3e433e8db1aa0df34dc
sektionschef <the.stevee@gmail.com>
2016-08-11 21:18:43 +0200

    test gitignore

--- credentials/credentials.py
+++ credentials/credentials.py
@@ -2,7 +2,7 @@
 
 ## get that shit here:  https://apps.twitter.com
 
-CONSUMER_KEY = 'Your-Consumer-Key'
+CONSUMER_KEY = 'manni'
 CONSUMER_SECRET = 'Your-Secret-Key'
 ACCESS_KEY = 'Your-Access-Key'
 ACCESS_SECRET = 'Your-Access-Secret'
