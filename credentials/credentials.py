5efbfe894e87e5129fa11b0edba82c17931cdc31
sektionschef <the.stevee@gmail.com>
2016-08-11 21:22:17 +0200

    removed credentials

--- credentials/credentials.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-## get that shit here:  https://apps.twitter.com
-
-CONSUMER_KEY = 'manni'
-CONSUMER_SECRET = 'Your-Secret-Key'
-ACCESS_KEY = 'Your-Access-Key'
-ACCESS_SECRET = 'Your-Access-Secret'
