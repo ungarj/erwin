e83eb79807c4e9cd809027c5d2443f1f57a6e088
sektionschef <the.stevee@gmail.com>
2016-08-11 23:29:08 +0200

    sau

--- corpus_delicti.py
+++ corpus_delicti.py
@@ -2,17 +2,35 @@
 
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, TweetTokenizer, PunktSentenceTokenizer
+import pandas as pd
 
 
-sample_text = "sample_text.txt"
+sample_text = "sample.txt"
 
 
 stop_words = set(stopwords.words('german'))
 #stop_words
 
 
-with open(sample_text):
-    sample = sample_text.read()
+with open(sample_text) as f:
+    sample = f.read()
+
+def tokenize_casual(sample_text):
+    word_tokens = TweetTokenizer(preserve_case=False, strip_handles=False, reduce_len=False).tokenize(sample_text)
+    #print(word_tokens)
+
+    #word_tokens = map(str.lower, word_tokens) ## to lowercase
+    #word_tokens = [ x.replace("[^A-Za-z0-9\u00F0-\u02AF#]+", "") for x in word_tokens ] #remove all which is not a letter or a number or an umlaut or a # , http://stackoverflow.com/questions/22017723/regex-for-umlaut
+    #word_tokens = [ x.replace('\n', "") for x in word_tokens ] ##remove newline
+
+    word_tokens = [w for w in word_tokens if not w in stop_words] ##remove stop words
+    return(pd.Series(word_tokens))
+
+tokenized = tokenize_casual(sample)
+#print(tokenized)
 
 
-#custom_sent_tokenizer = PunktSentenceTokenizer(sample_text)
+print(tokenized.value_counts().head(50))
+
+
+['blöße', ]
